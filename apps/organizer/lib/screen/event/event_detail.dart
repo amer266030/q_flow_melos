00d5a6a12f241ff49f41de99de261193a4323c80
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-20 21:08:36 +0300

    upd

--- apps/organizer/lib/screen/event/event_detail.dart
+++ apps/organizer/lib/screen/event/event_detail.dart
@@ -9,43 +9,109 @@ class AddEvent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: context.screenWidth * 0.2),
-            Padding(
-              padding: EdgeInsets.only(left: context.screenWidth * 0.08),
-              child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text('Add Event', style: context.titleLarge)),
-            ),
-            SizedBox(height: context.screenWidth * 0.03),
-            CircleAvatar(
-              radius: context.screenWidth * 0.3,
-              backgroundColor: context.primary,
-              child: Icon(
-                CupertinoIcons.calendar_badge_plus,
-                size: context.screenWidth * 0.2,
-                color: context.textColor1,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: context.screenWidth * 0.2),
+              Padding(
+                padding: EdgeInsets.only(left: context.screenWidth * 0.08),
+                child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text('Add Event', style: context.titleLarge)),
               ),
-            ),
-            SizedBox(height: context.screenWidth * 0.01),
-            TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Add Event',
-                  style: context.bodyMedium.copyWith(color: context.primary),
-                )),
-            SizedBox(height: context.screenWidth * 0.03),
-            Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: context.screenWidth * 0.08),
-              child: CustomTextField(
-                hintText: 'Event Name',
-                controller: TextEditingController(),
-                validation: (String value) {},
+              SizedBox(height: context.screenWidth * 0.03),
+              CircleAvatar(
+                radius: context.screenWidth * 0.3,
+                backgroundColor: context.primary,
+                child: Icon(
+                  Icons.person,
+                  size: context.screenWidth * 0.2,
+                  color: context.secondary,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: context.screenWidth * 0.01),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Add Event',
+                    style: context.bodyMedium.copyWith(color: context.primary),
+                  )),
+              SizedBox(height: context.screenWidth * 0.03),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  hintText: 'Event Name',
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.001),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  hintText: 'Description',
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.001),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  hintText: 'Start Date',
+                  suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.date_range_outlined)),
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.001),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  hintText: 'End Date',
+                  suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.date_range_outlined)),
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.001),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  hintText: 'Upload company .xlsx',
+                  suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.upload_file)),
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.001),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: context.screenWidth * 0.08),
+                child: CustomTextField(
+                  suffixIcon: IconButton(
+                      onPressed: () {}, icon: Icon(Icons.upload_file)),
+                  hintText: 'Upload visitors .xlsx',
+                  controller: TextEditingController(),
+                  validation: (String value) {},
+                ),
+              ),
+              SizedBox(height: context.screenWidth * 0.01),
+              PrimaryBtn(
+                callback: () {},
+                title: 'Create Event',
+              ),
+              SizedBox(height: context.screenWidth * 0.01),
+            ],
+          ),
         ),
       ),
     );
