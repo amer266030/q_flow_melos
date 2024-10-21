7f4746fa089c1ae0272bc22d3ef5935509d19597
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-10-21 19:23:41 +0300

    Done CompanyScreen

--- apps/organizer/lib/screen/rate/company/company_screen.dart
+++ apps/organizer/lib/screen/rate/company/company_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:my_melos_widgets/widgets.dart';
+
+import '../widget/custom_rating.dart';
 
 class CompanyScreen extends StatelessWidget {
   const CompanyScreen({super.key});
@@ -19,7 +22,49 @@ class CompanyScreen extends StatelessWidget {
             delegate: SliverChildListDelegate([
               SizedBox(height: context.screenHeight * 0.04),
               Text('ABC Company', style: context.titleLarge),
+              SizedBox(height: context.screenHeight * 0.02),
+              Text('Rating:', style: context.titleLarge),
               SizedBox(height: context.screenHeight * 0.04),
+              CustomRating(
+                  boldText: 'Overall Satisfaction: ',
+                  text:
+                      'How satisfied were you with the overall interview process?'),
+              CustomRating(
+                  boldText: 'Interviewer Professionalism: ',
+                  text:
+                      'How would you rate the professionalism of the interviewer?'),
+              CustomRating(
+                  boldText: 'Clarity of Questions: ',
+                  text:
+                      'Were the interview questions clear and relevant to the role?'),
+              CustomRating(
+                  boldText: 'Time Management: ',
+                  text:
+                      'Was the interview conducted within the scheduled time frame?'),
+              CustomRating(
+                  boldText: 'Fairness of the Process: ',
+                  text:
+                      'Do you feel that the interview process was fair and unbiased?'),
+              CustomRating(
+                  boldText: 'Opportunity to Express Skills: ',
+                  text:
+                      'Did you feel you had enough opportunity to showcase your skills and experience?'),
+              CustomRating(
+                  boldText: 'Interview Environment: ',
+                  text:
+                      'How comfortable was the interview setting (e.g., location, noise level, privacy)?'),
+              CustomRating(
+                  boldText: 'Communication: ',
+                  text:
+                      'How well were the interview instructions and expectations communicated prior to the interview?'),
+              CustomRating(
+                  boldText: 'Post-Interview Feedback: ',
+                  text:
+                      'How satisfied are you with the feedback (if provided) after the interview?'),
+              CustomRating(
+                  boldText: 'Likelihood to Recommend: ',
+                  text:
+                      'Based on your experience, how likely are you to recommend this interview process to others?'),
             ]),
           )
         ],
---
+++ apps/organizer/lib/screen/rate/widget/custom_rating.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:my_melos_widgets/widgets.dart';
+
+class CustomRating extends StatelessWidget {
+  const CustomRating({super.key, this.boldText, this.text});
+
+  final String? boldText;
+  final String? text;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        RichText(
+            text: TextSpan(children: [
+          TextSpan(
+            text: boldText,
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          TextSpan(text: text, style: TextStyle(color: Colors.grey))
+        ])),
+        SizedBox(height: context.screenHeight * 0.01),
+        Align(
+          alignment: Alignment.center,
+          child: RatingBarIndicator(
+            rating: 2.75,
+            itemBuilder: (context, index) => Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            itemCount: 5,
+            itemSize: 50,
+            direction: Axis.horizontal,
+          ),
+        ),
+        SizedBox(height: context.screenHeight * 0.01),
+        Divider(thickness: context.screenHeight * 0.003),
+      ],
+    );
+  }
+}
